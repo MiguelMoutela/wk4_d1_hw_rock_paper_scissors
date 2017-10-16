62cd08f826f6164afb3ed000efa32f87e2910c2c
Miguel <miguelmoutela@hotmail.com>
2017-10-16 17:41:10 +0100

    error - expecting 0 params but receiving 2

--- controller.rb
+++ controller.rb
@@ -1,7 +1,8 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require('.models/game.rb')
+require('./models/game.rb')
 
 get '/:shoot1/:shoot2' do
-  outcome = Game.new(params[:shoot1], params[:shoot2])
+  shoot = Game.new(params[:shoot1].to_s, params[:shoot2].to_s)
+  @play = shoot.rock_paper_scissors()
 end
--- models/game.rb
+++ models/game.rb
@@ -5,26 +5,32 @@ class Game
     @shoot2 = shoot2
   end
 
-  case rock_paper_scissors
-    when @shoot1 == @shoot2
-      return "It's a tie!!"
-    when @shoot1 = "rocks"
-      if @shoot2 == "scissors"
-        return "Rocks wins"
-      else
-        return "Rocks loses"
-      end
-    when @shoot1 = "scissors"
-      if @shoot2 == "rocks"
-        return "Scissors loses"
-      else
-        return "Scissors wins"
-      end
-    when @shoot1 = "paper"
-      if @shoot2 == "rock"
-        return "Paper wins"
-      else
-        return "Paper loses"
-      end
+  def rock_paper_scissors
+
+    case rock_paper_scissors
+      when @shoot1 == @shoot2
+        return "It's a tie"
+      when @shoot1 = "rock"
+        if @shoot2 == "scissors"
+          return "Rock wins"
+        else
+          return "Rock loses"
+        end
+
+      when @shoot1 = "scissors"
+        if @shoot2 == "rock"
+          return "Scissors loses"
+        else
+          return "Scissors wins"
+        end
+
+      when @shoot1 = "paper"
+        if @shoot2 == "rock"
+          return "Paper wins"
+        else
+          return "Paper loses"
+        end
+
+    end
   end
 end
